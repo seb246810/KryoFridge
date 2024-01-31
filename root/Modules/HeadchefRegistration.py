fe1f75ee1ed4bafeb82071b5cf03d2326acda273
jxtcas <126854983+jxtcas@users.noreply.github.com>
2024-01-31 14:26:21 +0000

    Head Chef Register is working

--- root/Modules/HeadchefRegistration.py
+++ root/Modules/HeadchefRegistration.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QLabel, QApplication, QMessageBox, QWidget
 from PyQt5 import uic,QtGui
+from Headcheflogin import *
 from fridge import *
 
 
@@ -7,13 +8,38 @@ class Headchefregister(QWidget):
     def __init__(self,parent = None):
         super(Headchefregister,self).__init__()
         uic.loadUi("../UI/HeadChefRegistration.ui",self)
+self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.confirmfield.setEchoMode(QtWidgets.QLineEdit.Password)
         self.show()
 
-        #self.Username.clicked.connect(self)
-        #self.Password.clicked.connect(self)
-        #self.Confirm.clicked.connect(self)
-        #self.BackButton.clicked.connect(self)
-        
+        self.BackButton.clicked.connect(self.back)
+        self.registerbutton.clicked.connect(self.registerFunction)
+
+    def registerFunction(self):
+        user = self.usernamefield.text()
+        password = self.passwordfield.text()
+        confirm = self.confirmfield.text()
+
+        if len(user)==0 or len(password)==0 or len(confirm)==0:
+            self.error.setText("Please fill in all inputs.")
+
+        elif password != confirm:
+            self.error.setText("Passwords do not match.")
+
+        else:
+            conn = sqlite3.connect('headchefuser.db')
+            cur = conn.cursor()
+
+
+            headchef_info = [user, password]
+            cur.execute('INSERT INTO users(username, password) VALUES (?,?)', headchef_info)
+
+            conn.commit()
+            conn.close()
+            self.close()
+
+    def back(self):
+        self.close()
 
 
         
